31469f725a7ea016d2a6ec66e69dace76939a426
sneezer77 <sneezer77@1821c7d2-850a-4518-87f4-f836794d6d55>
2013-02-22 03:01:25 +0000

    Id/pwd 저장하기 구현완료

--- trunk/src/main/java/com/sneezer/parkrio/MainActivity.java
+++ trunk/src/main/java/com/sneezer/parkrio/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,29 +14,59 @@ import android.widget.Toast;
 public class MainActivity extends AbstractAsyncActivity {
 	private static String TAG = "parkrio";
 
-	SharedPreferences preferences = getSharedPreferences("DATA_STORE", Service.MODE_PRIVATE);
-	
+	private EditText inputUsername;
+	private EditText inputPassword;
+
+	private SharedPreferences preferences;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// Log.i(TAG, "onCreate");
 		setContentView(R.layout.main);
-		
-		String username = this.preferences.getString("username","");
-		String password = this.preferences.getString("password","");
 
-		EditText inputUsername = (EditText) findViewById(R.id.useridEntry);
-		inputUsername.setText(username, TextView.BufferType.EDITABLE);
+		final SharedPreferences preferences = getSharedPreferences("USER_INFO",
+				MODE_PRIVATE);
+
+		String username = preferences.getString("username", "");
+		String password = preferences.getString("password", "");
+		boolean isRemember = preferences.getBoolean("isRemember", false);
+
+		final EditText inputUsername = (EditText) findViewById(R.id.useridEntry);
+		final EditText inputPassword = (EditText) findViewById(R.id.passwordEntry);
+		final CheckBox rememberChk = (CheckBox) findViewById(R.id.rememberChk);
 		
-		EditText inputPassword = (EditText) findViewById(R.id.passwordEntry);
-		inputPassword.setText(password, TextView.BufferType.EDITABLE);
+		// 저장되어 있는 userid/password 를 셋팅
+		if (isRemember) {
+			inputUsername.setText(username, TextView.BufferType.EDITABLE);
+			inputPassword.setText(password, TextView.BufferType.EDITABLE);
+			rememberChk.setChecked(isRemember);
+		}
 		
+
 		Button loginBtn = (Button) findViewById(R.id.loginBtn);
 		loginBtn.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
+				if (((CheckBox) findViewById(R.id.rememberChk)).isChecked()) {
 
+					SharedPreferences.Editor editor = preferences.edit();
+					editor.putString("username", inputUsername.getText().toString());
+					editor.putString("password", inputPassword.getText().toString());
+					editor.putBoolean("isRemember", rememberChk.isChecked());
+					editor.commit();
+				}
+
+			}
+		});
+
+		Button exitBtn = (Button) findViewById(R.id.exitBtn);
+		exitBtn.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				finish();
 			}
 		});
 	}
@@ -59,7 +90,7 @@ public class MainActivity extends AbstractAsyncActivity {
 			// TODO Auto-generated method stub
 			return null;
 		}
-		
+
 		@Override
 		protected void onPostExecute(Message result) {
 			dismissProgressDialog();
