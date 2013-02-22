0b2ce702b038e9fee5fb4381718b55aa433610ed
sneezer77 <sneezer77@naver.com>
2013-02-22 08:04:19 +0000

    restTemplate 구현중

--- src/main/java/com/sneezer/parkrio/MainActivity.java
+++ src/main/java/com/sneezer/parkrio/MainActivity.java
@@ -1,5 +1,8 @@
 package com.sneezer.parkrio;
 
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
 import android.app.Service;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
@@ -35,14 +38,13 @@ public class MainActivity extends AbstractAsyncActivity {
 		final EditText inputUsername = (EditText) findViewById(R.id.useridEntry);
 		final EditText inputPassword = (EditText) findViewById(R.id.passwordEntry);
 		final CheckBox rememberChk = (CheckBox) findViewById(R.id.rememberChk);
-		
+
 		// 저장되어 있는 userid/password 를 셋팅
 		if (isRemember) {
 			inputUsername.setText(username, TextView.BufferType.EDITABLE);
 			inputPassword.setText(password, TextView.BufferType.EDITABLE);
 			rememberChk.setChecked(isRemember);
 		}
-		
 
 		Button loginBtn = (Button) findViewById(R.id.loginBtn);
 		loginBtn.setOnClickListener(new View.OnClickListener() {
@@ -52,11 +54,16 @@ public class MainActivity extends AbstractAsyncActivity {
 				if (((CheckBox) findViewById(R.id.rememberChk)).isChecked()) {
 
 					SharedPreferences.Editor editor = preferences.edit();
-					editor.putString("username", inputUsername.getText().toString());
-					editor.putString("password", inputPassword.getText().toString());
+					editor.putString("username", inputUsername.getText()
+							.toString());
+					editor.putString("password", inputPassword.getText()
+							.toString());
 					editor.putBoolean("isRemember", rememberChk.isChecked());
 					editor.commit();
+					Toast.makeText(getApplicationContext(), "committed", Toast.LENGTH_LONG).show();
 				}
+				//Toast.makeText(getApplicationContext(), "FetchSecuredResourceTask", Toast.LENGTH_LONG).show();
+				new FetchSecuredResourceTask().execute();
 
 			}
 		});
@@ -79,10 +86,23 @@ public class MainActivity extends AbstractAsyncActivity {
 			AsyncTask<Void, Void, Message> {
 		private String username;
 		private String password;
-
+					
 		@Override
 		protected void onPreExecute() {
+			String base_url = getString(R.string.base_url);
+						
+			EditText editText = (EditText) findViewById(R.id.useridEntry);
+			this.username = editText.getText().toString();
+			editText = (EditText) findViewById(R.id.passwordEntry);
+			this.password = editText.getText().toString();
 
+			Toast.makeText(getApplicationContext(), "id="+this.username+"/pwd="+this.password, Toast.LENGTH_LONG).show();
+			
+			RestTemplate restTemplate = new RestTemplate();
+			restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+			String response = restTemplate.getForObject(base_url, String.class, "");
+			
+			
 		}
 
 		@Override
@@ -93,8 +113,9 @@ public class MainActivity extends AbstractAsyncActivity {
 
 		@Override
 		protected void onPostExecute(Message result) {
-			dismissProgressDialog();
-			displayResponse(result);
+			//dismissProgressDialog();
+			//displayResponse(result);
+			
 		}
 
 	}
--- target/generated-sources/r/com/sneezer/parkrio/R.java
+++ target/generated-sources/r/com/sneezer/parkrio/R.java
@@ -29,11 +29,12 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int exitBtnText=0x7f040006;
-        public static final int loginBtnText=0x7f040005;
-        public static final int loginTitle=0x7f040001;
-        public static final int passwordLabel=0x7f040003;
-        public static final int rememberidLabel=0x7f040004;
-        public static final int usernameLabel=0x7f040002;
+        public static final int base_url=0x7f040001;
+        public static final int exitBtnText=0x7f040007;
+        public static final int loginBtnText=0x7f040006;
+        public static final int loginTitle=0x7f040002;
+        public static final int passwordLabel=0x7f040004;
+        public static final int rememberidLabel=0x7f040005;
+        public static final int usernameLabel=0x7f040003;
     }
 }
