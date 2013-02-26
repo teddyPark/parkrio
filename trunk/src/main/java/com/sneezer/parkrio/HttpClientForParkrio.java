29e0a2a97900ba3aaac4c16757a6e5d196111cdf
sneezer77 <sneezer77@1821c7d2-850a-4518-87f4-f836794d6d55>
2013-02-26 09:32:18 +0000

    일간 데이터 가져오기 성공

---
+++ trunk/src/main/java/com/sneezer/parkrio/HttpClientForParkrio.java
@@ -0,0 +1,61 @@
+package com.sneezer.parkrio;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import org.apache.http.HttpResponse;
+
+import android.util.Log;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+
+public class HttpClientForParkrio {
+
+	public static String fetch(URL url, String cookieStr, String postParam) {
+
+		HttpResponse response = null;
+		HttpURLConnection httpClient = null;
+		StringBuilder html = new StringBuilder();
+
+		// set http client
+		try {
+			httpClient = (HttpURLConnection) url.openConnection();
+			httpClient.setRequestMethod("POST");
+			httpClient.setRequestProperty("Referer",
+					"http://211.49.175.211/hwork/iframe_DayValue.aspx");
+			httpClient
+					.setRequestProperty(
+							"User-Agent",
+							"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1;; APCPMS=E839.353E.2E0A-win7^N20120502090046254556C65BBCE3E22DEE3F_1981^1^1^2186362890^30282584^2160947136; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; TCO_20130225130629)");
+			httpClient.setRequestProperty("Cookie",cookieStr);
+
+			OutputStream opstrm = httpClient.getOutputStream();
+			opstrm.write(postParam.getBytes());
+			opstrm.flush();
+			opstrm.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(httpClient.getInputStream()));
+			while (true) {
+				String line = br.readLine();
+				if (line == null)
+					break;
+				html.append(line + '\n');
+				Log.i("readHTML", line);
+			}
+			br.close();
+			httpClient.disconnect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return html.toString();
+	}
+}
--- trunk/src/main/java/com/sneezer/parkrio/Measurement.java
+++ trunk/src/main/java/com/sneezer/parkrio/Measurement.java
@@ -1,40 +1,56 @@
 package com.sneezer.parkrio;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Measurement {
-	private int elect = 0;	// 전기
-	private int hotwater = 0;	// 온수
-	private int heat = 0;	// 난방
-	private int water = 0;	// 수도
-	private int gas = 0;	// 가스
+	private float elect = 0;	// 전기
+	private float hotwater = 0;	// 온수
+	private float heat = 0;	// 난방
+	private float water = 0;	// 수도
+	private float gas = 0;	// 가스
 	
-	public int getElect() {
+	public float getElect() {
 		return elect;
 	}
-	public void setElect(int elect) {
+	public void setElect(float elect) {
 		this.elect = elect;
 	}
-	public int getHotwater() {
+	public float getHotwater() {
 		return hotwater;
 	}
-	public void setHotwater(int hotwater) {
+	public void setHotwater(float hotwater) {
 		this.hotwater = hotwater;
 	}
-	public int getHeat() {
+	public float getHeat() {
 		return heat;
 	}
-	public void setHeat(int heat) {
+	public void setHeat(float heat) {
 		this.heat = heat;
 	}
-	public int getWater() {
+	public float getWater() {
 		return water;
 	}
-	public void setWater(int water) {
+	public void setWater(float water) {
 		this.water = water;
 	}
-	public int getGas() {
+	public float getGas() {
 		return gas;
 	}
-	public void setGas(int gas) {
+	public void setGas(float gas) {
 		this.gas = gas;
+	}
+	public String toString() {
+		return (new String(this.elect+";"+this.hotwater+";"+this.heat+";"+this.water+";"+this.gas));
+	}
+	public Map<String, Float> getObject(){
+		Map<String, Float> resultMap = new HashMap<String,Float>();
+		resultMap.put("Elect", elect);
+		resultMap.put("Hotwater", hotwater);
+		resultMap.put("Heat", heat);
+		resultMap.put("Water", water);
+		resultMap.put("Gas", gas);
+		
+		return resultMap;
 	}
 }
